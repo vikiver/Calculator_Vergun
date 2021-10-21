cc81d7cd2d98ae992cd945d3d800db42c5ca99f6
vikiver <91251250+vikiver@users.noreply.github.com>
2021-10-21 11:24:45 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+print("Введите первое число")
+a = int(input())
+print("Введите второе число")
+d = int(input())
+
+print(a, "+", d, "=", a+d)
+print(a, "-", d, "=", a-d)
+print(d, "-", a, "=", d-a)
+print(a, "*", d, "=", a*d)
+print(a, "/", d, "=", a/d)
+print(d, "/", a, "=", d/a)
+print(a, "^", d, "=", a**d)
+print(d, "^", a, "=", d**a)
